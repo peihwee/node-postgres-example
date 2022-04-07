5b1a279351ad2e372f903ab8877cde6b10fdac37
peihwee <puaywee@gmail.com>
2022-04-07 16:43:14 +0800

    test commit

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const pg = require('pg');
+
+///////////////////////////////////////////////////////////////////////////
+// DATABASE_URL extracted from .env file
+///////////////////////////////////////////////////////////////////////////
+const dbConfig = { connectionString: process.env.DATABASE_URL };
+
+
+const pool = new pg.Pool({
+    ...dbConfig,
+    max: process.env.MAX_CONNECTION || 5,
+});
+
+module.exports = pool;
